e4c33cf214b2ebef8b9b5e449f6673c4e1bcbad3
Kohinoor <kohinoorkapali@27gmail.com>
2025-12-23 19:56:39 +0545

    migration to vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { useState } from 'react'
+import logo from './assets/Logo.png'
+import ViewDetail_Bottom from './components/ViewDetail/ViewDetail_Bottom';
+
+function App() {
+  return (
+    <div className="App">
+      <ViewDetail_Bottom/>
+    </div>
+  );
+}
+
+export default App
