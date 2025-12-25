ea1f45a59361d8ab2dd19bb930a0c12e04acf513
Kohinoor <kohinoorkapali@27gmail.com>
2025-12-25 11:37:33 +0545

    Successfully navigate to Login & Register from LandingPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 
 import './App.css';
 import { useState } from 'react'
-import logo from './assets/Logo.png'
 import LandingPage from './components/LandingPage/Landingpage';
+import {Login} from './components/Login/Login';
+import {Register} from './components/Register/Register';
 
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -12,7 +13,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
