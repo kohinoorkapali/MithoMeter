a9ba4b32fb98187e30659c3928d5d540fb5f25e7
Dawn <bistanshu3@gmail.com>
2025-12-25 16:50:26 +0545

    added View Analytics in Admin Dashboard

--- src/components/AdminDashboard/AdminDashboard.jsx
+++ src/components/AdminDashboard/AdminDashboard.jsx
@@ -1,10 +1,15 @@
 import { Header } from '../Header.jsx';
 import './AdminDashboard.css'
+import { Analytics } from './ViewAnalytics/Analytics.jsx';
 
 export function AdminDashboard(){
     return(
         <>
         <Header role = "admin"/>
+        <div className='dashboard-container'>
+            <Analytics/>
+        </div>
+        
         </>
     );
 }
