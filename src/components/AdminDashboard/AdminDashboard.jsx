d0e21c29afafc07de2217ee5be07634d0abfcc17
Dawn <bistanshu3@gmail.com>
2025-12-25 17:02:50 +0545

    added users and reports list in Admin Dashboard

--- src/components/AdminDashboard/AdminDashboard.jsx
+++ src/components/AdminDashboard/AdminDashboard.jsx
@@ -1,6 +1,10 @@
-import { Header } from '../Header.jsx';
 import './AdminDashboard.css'
 import { Analytics } from './ViewAnalytics/Analytics.jsx';
+import ViewReview from './ViewReview/ViewReview.jsx';
+import ViewUser from './ViewUser/ViewUser.jsx';
+
+import { Header } from '../Header.jsx';
+
 
 export function AdminDashboard(){
     return(
@@ -8,6 +12,11 @@ export function AdminDashboard(){
         <Header role = "admin"/>
         <div className='dashboard-container'>
             <Analytics/>
+            <div className='user-report'>
+                <ViewUser/>
+                <ViewReview/>
+            </div>
+
         </div>
         
         </>
